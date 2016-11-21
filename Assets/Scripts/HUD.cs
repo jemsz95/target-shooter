0b73e5e4c8a852d14960374d0ec82302e7587410
Javier Meza <javenmeza@gmail.com>
2016-11-21 00:31:10 -0600

    Start notification system

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -1,27 +1,64 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class HUD : MonoBehaviour {
+  
+  private struct Message {
+    public string content;
+    public float time;
+
+    public Message(string c, float t) {
+      content = c;
+      time = t;
+    }
+  }
+
   public Text ActionLabel;
   public Text ScoreLabel;
   public Text TimeLabel;
+  public Text GameStatusLabel;
 
   public Interactor MainInteractor;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+  private LinkedList<Message> notifications;
+
+  public void Notify(string message, float time) {
+    StartCoroutine(AddNotification(new Message(message, time)))
+  }
+
+  // Use this for initialization
+  void Start () {
+    Notify("Advance to counter to start.", 3.0f);
+  }
+  
+  // Update is called once per frame
+  void Update () {
     TimeLabel.text = "Time: " + GameManager.Instance.TimeLeft;
     ScoreLabel.text = "Score: " + GameManager.Instance.Points;
-	  if(MainInteractor.Target != null) {
+
+    if(MainInteractor.Target != null) {
       ActionLabel.text = "Press E to " + MainInteractor.Target.Action;
     } else {
       ActionLabel.text = "";
     }
-	}
+  }
+
+  IEnumerator AddNotification(Message m) {
+    if(notifications.Count == 0) {
+      notifications.AddBack(m);
+
+      while(notifications.Count > 0) {
+        Message m = notifications.First;
+        notifications.RemoveFirst();
+        GameStatusLabel.text = m.content;
+        yield return new WaitForSeconds(m.time);
+      }
+
+      GameStatusLabel.text = "";
+    } else {
+      notidications.AddBack(m);
+    }
+  }
 }
